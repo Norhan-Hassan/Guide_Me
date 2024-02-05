fa3520ed6a38590269e2860e79701ad20a3a7fb9
Norhan Hassan <nwrhanhsnly@gmail.com>
2024-02-05 23:44:34 +0200

    add tourist and admin controllers

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guide_Me.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+    }
+}
