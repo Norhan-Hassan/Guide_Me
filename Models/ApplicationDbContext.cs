59454fa84640145ad302ca243210790da8727ce6
Norhan Hassan <nwrhanhsnly@gmail.com>
2024-02-08 00:02:31 +0200

    plave media

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -19,12 +19,22 @@ namespace Guide_Me.Models
             // Configure your entity mappings here
             modelBuilder.Entity<Place>().HasKey(p => p.Id);
             modelBuilder.Entity<City>().HasKey(c => c.Id);
+            modelBuilder.Entity<PlaceMedia>().HasKey(placemedia => placemedia.Id);
 
             // Define relationships (if any)
             modelBuilder.Entity<Place>()
                 .HasOne(p => p.City)
                 .WithMany(c => c.Places)
                 .HasForeignKey(p => p.CityId);
+
+            modelBuilder.Entity<Place>()
+                .HasMany(p=>p.PlaceMedias)
+                .WithOne(placemedia=> placemedia.Place)
+                .HasForeignKey(pm => pm.PlaceId);
+
+
+
+
             base.OnModelCreating(modelBuilder);
         }
 
