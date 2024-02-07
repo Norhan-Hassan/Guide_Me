f013e282433895a5a61633c91d83663743881c90
alaaessamali <119875178+alaaessamali@users.noreply.github.com>
2024-02-07 18:01:49 +0200

    DbContext

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Guide_Me.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 
+using Guide_Me.Models;
+
 namespace Guide_Me
 {
     public class Program
@@ -13,7 +15,8 @@ namespace Guide_Me
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+           builder.services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
