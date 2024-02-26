0d8f70eef7eff7288bbc2a72fcef7069bdfb2a82
Norhan Hassan <nwrhanhsnly@gmail.com>
2024-02-27 01:46:38 +0200

    some changes

--- Program.cs
+++ Program.cs
@@ -25,17 +25,28 @@ namespace Guide_Me
             builder.Services.AddScoped<ICityService, CityService>();
             builder.Services.AddScoped<IPlaceService, PlaceService>();
             builder.Services.AddHttpContextAccessor();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
             app.UseStaticFiles();
 
             app.UseAuthorization();
+            app.UseCors("AllowAll");
 
 
             app.MapControllers();
