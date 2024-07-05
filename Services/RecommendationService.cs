ec1e430e85a04fe6cfcc9f93edb4ca785adeb882
Norhan Hassan <nwrhanhsnly@gmail.com>
2024-07-05 04:28:48 +0300

    update recommendation

--- Services/RecommendationService.cs
+++ Services/RecommendationService.cs
@@ -1,6 +1,7 @@
 ﻿using Guide_Me.DTO;
 using Guide_Me.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Guide_Me.Services
@@ -13,13 +14,15 @@ namespace Guide_Me.Services
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IBlobStorageService _blobStorageService;
         private readonly ITranslationService _translationService;
+        private readonly ILogger<RecommendationService> _logger;
 
         public RecommendationService(ApplicationDbContext context,
             ITouristService touristService,
-            IPlaceService placeService, 
-            IHttpContextAccessor httpContextAccessor, 
+            IPlaceService placeService,
+            IHttpContextAccessor httpContextAccessor,
             IBlobStorageService blobStorageService,
-            ITranslationService translationService)
+            ITranslationService translationService,
+            ILogger<RecommendationService> logger)
         {
             _context = context;
             _touristService = touristService;
@@ -27,134 +30,164 @@ namespace Guide_Me.Services
             _httpContextAccessor = httpContextAccessor;
             _blobStorageService = blobStorageService;
             _translationService = translationService;
+            _logger = logger;
         }
 
         public List<PlaceRecommendationDto> GetRecommendation(string touristName, string cityName, string placeName)
         {
-            // Retrieve the tourist's details
-            var tourist = _context.Tourist.FirstOrDefault(t => t.UserName == touristName);
-            if (tourist == null)
+            try
             {
-                return null; // Tourist not found
-            }
-
-            var preferredLanguage = tourist.Language;
-
-            // Translate the place name and city name to English if the preferred language is not English
-            string cityNameToSearch = cityName;
-            string placeNameToSearch = placeName;
-            if (preferredLanguage != "en")
-            {
-                cityNameToSearch = _translationService.TranslateTextResultASync(cityName, "en");
-                placeNameToSearch = _translationService.TranslateTextResultASync(placeName, "en");
-            }
-
-            string touristID = _touristService.GetUserIdByUsername(touristName);
-            int placeId = _placeService.GetPlaceIdByPlaceName(placeNameToSearch);
-
-            if (!string.IsNullOrEmpty(touristID) && placeId != 0)
-            {
-                var favPlaces = _context.Favorites
-                                        .Where(f => f.TouristID == touristID && f.Place.City.CityName == cityNameToSearch)
-                                        .Select(f => f.Place)
-                                        .ToList();
-
-                var histPlaces = _context.histories
-                                         .Where(h => h.TouristId == touristID && h.Place.City.CityName == cityNameToSearch)
-                                         .Select(h => h.Place)
-                                         .ToList();
-
-                double Latitude = _context.Places.Where(p => p.Id == placeId).Select(p => p.latitude).FirstOrDefault();
-                double Longitude = _context.Places.Where(p => p.Id == placeId).Select(p => p.longitude).FirstOrDefault();
-
-                var nearestPlaces = _context.Places
-                               .Where(p => p.City.CityName == cityNameToSearch && p.PlaceName != placeNameToSearch)
-                               .Select(p => new
-                               {
-                                   Place = p,
-                                   Distance = 6371 * Math.Acos(
-                                       Math.Cos(Latitude * Math.PI / 180) * Math.Cos(p.latitude * Math.PI / 180) *
-                                       Math.Cos((p.longitude - Longitude) * Math.PI / 180) +
-                                       Math.Sin(Latitude * Math.PI / 180) * Math.Sin(p.latitude * Math.PI / 180))
-                               })
-                               .OrderBy(p => p.Distance)
-                               .Take(3)
-                               .ToList()
-                               .Select(p => new PlaceRecommendationDto
-                               {
-                                   PlaceName = preferredLanguage != "en"
-                                       ? _translationService.TranslateTextResultASync(p.Place.PlaceName, preferredLanguage)
-                                       : p.Place.PlaceName,
-                                   Image = p.Place.PlaceMedias?.FirstOrDefault(m => m.MediaType.ToLower() == "image")?.MediaContent != null
-                                           ? _blobStorageService.GetBlobUrlmedia(p.Place.PlaceMedias
-                                               ?.FirstOrDefault(m => m.MediaType.ToLower() == "image")
-                                               ?.MediaContent)
-                                           : null,
-                                   Rate = _context.Rating
-                                          .Where(r => r.PlaceId == p.Place.Id)
-                                           .Select(r => r.Rate)
-                                           .FirstOrDefault()
-                               })
-                               .ToList();
-
-                var commonPlaces = favPlaces.Concat(histPlaces).ToList();
-
-                if (commonPlaces.Any())
+               
+                // Retrieve the tourist's details
+                var tourist = _context.Tourist.FirstOrDefault(t => t.UserName == touristName);
+                if (tourist == null)
                 {
-                    var categoryCounts = commonPlaces
-                        .GroupBy(p => p.Category)
-                        .Select(g => new { Category = g.Key, Count = g.Count() })
-                        .OrderByDescending(g => g.Count)
-                        .ToList();
+                    
+                    return null; // Tourist not found
+                }
 
-                    var mostFrequentCategory = categoryCounts.FirstOrDefault()?.Category?.ToLower();
+                var preferredLanguage = tourist.Language;
+            
 
-                    if (!string.IsNullOrEmpty(mostFrequentCategory))
-                    {
-                        var allPlacesInCity = _context.Places
-                                                      .Include(p => p.PlaceMedias)
-                                                      .Where(p => p.City.CityName == cityNameToSearch && p.Category.ToLower() == mostFrequentCategory)
-                                                      .ToList();
+                // Translate the place name and city name to English if the preferred language is not English
+                string cityNameToSearch = cityName;
+                string placeNameToSearch = placeName;
+                if (preferredLanguage != "en")
+                {
+                    cityNameToSearch = _translationService.TranslateTextResultASync(cityName, "en");
+                    placeNameToSearch = _translationService.TranslateTextResultASync(placeName, "en");
+                  
+                }
 
-                        var recommendedPlaces = allPlacesInCity
-                                                 .Where(place => !favPlaces.Contains(place) && !histPlaces.Contains(place) && place.PlaceName != placeNameToSearch)
-                                                 .OrderBy(p => Guid.NewGuid())
-                                                 .Select(place => {
-                                                     var mediaContent = place.PlaceMedias
-                                                                             ?.FirstOrDefault(m => m.MediaType.ToLower() == "image")
-                                                                             ?.MediaContent;
+                string touristID = _touristService.GetUserIdByUsername(touristName);
+                int placeId = _placeService.GetPlaceIdByPlaceName(placeNameToSearch);
 
-                                                     return new PlaceRecommendationDto
-                                                     {
-                                                         PlaceName = preferredLanguage != "en"
-                                                             ? _translationService.TranslateTextResultASync(place.PlaceName, preferredLanguage)
-                                                             : place.PlaceName,
-                                                         Image = mediaContent != null
-                                                                 ? _blobStorageService.GetBlobUrlmedia(mediaContent)
-                                                                 : null,
-                                                         Rate = _context.Rating
-                                                            .Where(r => r.PlaceId == place.Id)
-                                                            .Select(r => r.Rate)
-                                                            .FirstOrDefault()
-                                                     };
-                                                 })
+                if (!string.IsNullOrEmpty(touristID) && placeId != 0)
+                {
+                    // Retrieve favorite places of the tourist
+                    var favPlaces = _context.Favorites
+                                            .Where(f => f.TouristID == touristID && f.Place.City.CityName == cityNameToSearch)
+                                            .Select(f => f.Place)
+                                            .ToList();
+                  
+
+                    // Retrieve historical places visited by the tourist
+                    var histPlaces = _context.histories
+                                                .Where(h => h.TouristId == touristID && h.Place.City.CityName == cityNameToSearch)
+                                                .Select(h => h.Place)
+                                                .ToList();
+                    
+
+                    // Retrieve latitude and longitude of the requested place
+                    double Latitude = _context.Places.Where(p => p.Id == placeId).Select(p => p.latitude).FirstOrDefault();
+                    double Longitude = _context.Places.Where(p => p.Id == placeId).Select(p => p.longitude).FirstOrDefault();
+
+                    // Retrieve nearest places excluding the requested place
+                    var nearestPlaces = _context.Places
+                                                .Where(p => p.City.CityName == cityNameToSearch && p.PlaceName != placeNameToSearch)
+                                                .Include(p => p.PlaceMedias)
+                                                .Select(p => new
+                                                {
+                                                    Place = p,
+                                                    Distance = 6371 * Math.Acos(
+                                                        Math.Cos(Latitude * Math.PI / 180) * Math.Cos(p.latitude * Math.PI / 180) *
+                                                        Math.Cos((p.longitude - Longitude) * Math.PI / 180) +
+                                                        Math.Sin(Latitude * Math.PI / 180) * Math.Sin(p.latitude * Math.PI / 180))
+                                                })
+                                                .OrderBy(p => p.Distance)
                                                 .Take(3)
+                                                .ToList()
+                                                .Select(p => new PlaceRecommendationDto
+                                                {
+                                                    PlaceName = preferredLanguage != "en"
+                                                                ? _translationService.TranslateTextResultASync(p.Place.PlaceName, preferredLanguage)
+                                                                : p.Place.PlaceName,
+                                                    Image = GetPlaceImageUrl(p.Place),
+                                                    Rate = _context.Rating
+                                                                .Where(r => r.PlaceId == p.Place.Id)
+                                                                .Select(r => r.Rate)
+                                                                .FirstOrDefault()
+                                                })
                                                 .ToList();
 
-                        var combinedPlaces = recommendedPlaces.Concat(nearestPlaces)
-                                             .Where(c => c.PlaceName != placeNameToSearch)
-                                             .GroupBy(p => p.PlaceName)
-                                             .Select(g => g.First())
-                                             .ToList();
+                    // Combine favorite and historical places
+                    var commonPlaces = favPlaces.Concat(histPlaces).ToList();
 
-                        return combinedPlaces;
+                    // Determine the most frequent category among common places
+                    if (commonPlaces.Any())
+                    {
+                        var categoryCounts = commonPlaces
+                                                .GroupBy(p => p.Category)
+                                                .Select(g => new { Category = g.Key, Count = g.Count() })
+                                                .OrderByDescending(g => g.Count)
+                                                .ToList();
+
+                        var mostFrequentCategory = categoryCounts.FirstOrDefault()?.Category?.ToLower();
+
+                        if (!string.IsNullOrEmpty(mostFrequentCategory))
+                        {
+                            // Retrieve all places in the city with the most frequent category
+                            var allPlacesInCity = _context.Places
+                                                            .Include(p => p.PlaceMedias)
+                                                            .Where(p => p.City.CityName == cityNameToSearch && p.Category.ToLower() == mostFrequentCategory)
+                                                            .ToList();
+
+                            // Select recommended places not in favorites or history and not the requested place
+                            var recommendedPlaces = allPlacesInCity
+                                                        .Where(place => !favPlaces.Contains(place) && !histPlaces.Contains(place) && place.PlaceName != placeNameToSearch)
+                                                        .OrderBy(p => Guid.NewGuid())
+                                                        .Select(place => new PlaceRecommendationDto
+                                                        {
+                                                            PlaceName = preferredLanguage != "en"
+                                                                        ? _translationService.TranslateTextResultASync(place.PlaceName, preferredLanguage)
+                                                                        : place.PlaceName,
+                                                            Image = GetPlaceImageUrl(place),
+                                                            Rate = _context.Rating
+                                                                        .Where(r => r.PlaceId == place.Id)
+                                                                        .Select(r => r.Rate)
+                                                                        .FirstOrDefault()
+                                                        })
+                                                        .Take(3)
+                                                        .ToList();
+
+                            // Combine recommended and nearest places, exclude requested place
+                            var combinedPlaces = recommendedPlaces.Concat(nearestPlaces)
+                                                                .Where(c => c.PlaceName != placeNameToSearch)
+                                                                .GroupBy(p => p.PlaceName)
+                                                                .Select(g => g.First())
+                                                                .ToList();
+
+                            return combinedPlaces;
+                        }
                     }
+
                 }
+            }
+            catch (Exception ex)
+            {
+              
+                throw; // Rethrow the exception to propagate it up
             }
 
             return null;
         }
 
+        private string GetPlaceImageUrl(Place place)
+        {
+            if (place?.PlaceMedias == null)
+            {
+                return null;
+            }
 
+            var mediaContent = place.PlaceMedias.FirstOrDefault(m => m.MediaType.ToLower() == "image")?.MediaContent;
+            if (mediaContent != null)
+            {
+                return _blobStorageService.GetBlobUrlmedia(mediaContent);
+            }
+            else
+            {  
+                return null;
+            }
+        }
     }
 }
